8de9de682bbba866e853d0eeb113d57500282fe3
Makoto Kurihara <mkuri8m@gmail.com>
2019-06-05 23:05:57 +0900

    feat: Add the gatsby-plugin-web-font-loader
    Use google fonts "Libre Barcode 128 Text" for the site logo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,14 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Libre Barcode 128 Text']
+        }
+      }
+    },
   ],
 }
