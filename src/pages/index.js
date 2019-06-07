8f40f44dc7b22d12070e1e833f7f2ac098f5e347
Makoto Kurihara <mkuri8m@gmail.com>
2019-06-07 18:25:41 +0900

    feat: Add the GraphQL query to fetch the posts on contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <p>機械学習や自動運転に興味のある自動車メーカーエンジニアのブログです。</p>
@@ -13,8 +13,29 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    {data.allContentfulBlogPost.edges.map(({ node }) => (
+      <p>{node.title}</p>
+    ))}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allContentfulBlogPost(filter: {node_locale: {eq: "ja-JP"}}, sort: {fields: dateUpdated, order: DESC}) {
+      edges {
+        node {
+          title
+          slug
+          dateCreated
+          dateUpdated
+          tags {
+            name
+          }
+        }
+      }
+    }
+  }
+`
